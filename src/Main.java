30242cf987b2c38407d176ed3bb02f551849a030
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2011-01-16 03:31:32 +0100

    keys

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,32 @@ import org.apache.commons.lang.time.DateUtils;
 
 public class Main {
 	public static void main(String[] args) {
-		DataCollector invfizInvestorsPl = new InvestorsPlDataCollector("investor-fiz");
-		List<Data> investorsPlData = invfizInvestorsPl.collectData();
-		Date start = investorsPlData.get(0).getDate();
-		Date today = new Date(System.currentTimeMillis());
-		Date end = DateUtils.truncate(today, Calendar.DAY_OF_MONTH);
-		DataCollector invfiz = new StooqHistoricalDataCollector("invfiz", start, end, StooqHistoricalDataInterval.Daily);
-		List<Data> stooqHistData = invfiz.collectData();
-		List<Data[]> matched = DataUtils.matchByDate(investorsPlData, stooqHistData);
-		for (Iterator<Data[]> iterator = matched.iterator(); iterator.hasNext();) {
-			Data[] datas = (Data[]) iterator.next();
-			float v1 = datas[0].getValue();
-			String value1 = Float.toString(v1);
-			String value2 = "";
-			String ratio = "";
-			if (datas[1] != null) {
-				float v2 = datas[1].getValue();
-				value2 = Float.toString(v2);
-				ratio = Float.toString(v2/v1);
+		if (args[0].startsWith("inv")) {
+			String[] keys = InvestorsPlFactory.createKeys(args[0]);
+			DataCollector invfizInvestorsPl = new InvestorsPlDataCollector(keys[0]);
+			List<Data> investorsPlData = invfizInvestorsPl.collectData();
+			Date start = investorsPlData.get(0).getDate();
+			Date today = new Date(System.currentTimeMillis());
+			Date end = DateUtils.truncate(today, Calendar.DAY_OF_MONTH);
+			DataCollector invfiz = new StooqHistoricalDataCollector(keys[1], start, end, StooqHistoricalDataInterval.Daily);
+			List<Data> stooqHistData = invfiz.collectData();
+			List<Data[]> matched = DataUtils.matchByDate(investorsPlData, stooqHistData);
+			for (Iterator<Data[]> iterator = matched.iterator(); iterator.hasNext();) {
+				Data[] datas = (Data[]) iterator.next();
+				float v1 = datas[0].getValue();
+				String value1 = Float.toString(v1);
+				String value2 = "";
+				String ratio = "";
+				if (datas[1] != null) {
+					float v2 = datas[1].getValue();
+					value2 = Float.toString(v2);
+					ratio = Float.toString(v2 / v1);
+				}
+				// TODO: to file, floats with ',', separate by ';'
+				System.out.println(datas[0].getFormattedDate() + "," + value1 + ","	+ value2 + "," + ratio);
 			}
-			// TODO: to file, floats with ',', separate by ';'
-			System.out.println(datas[0].getFormattedDate() + "," + value1 + ","	+ value2 + "," + ratio);
+		} else if (args[0].equals("silver")){
+			// TODO:
 		}
 	}
 }
