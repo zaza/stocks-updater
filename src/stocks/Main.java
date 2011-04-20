f0d054d7465c158205bc11ff54a3b999706ae499
zaza <zaza@T60>
2011-04-20 20:11:59 +0200

    args

--- src/stocks/Main.java
+++ src/stocks/Main.java
@@ -29,62 +29,78 @@ public class Main {
 
 		// === investors.pl
 		for (Fund fund : Fund.values()) {
-			DataCollector invfizInvestorsCollector = new InvestorsPlDataCollector(fund.getInvestorsPl());
-			List<Data> invfizPl = invfizInvestorsCollector.collectData();
-			Date start = invfizPl.get(0).getDate();
-			Date today = new Date(System.currentTimeMillis());
-			Date end = DateUtils.truncate(today, Calendar.DAY_OF_MONTH);
-			DataCollector invfiz = new StooqHistoricalDataCollector(fund.getStooq(), start, end, StooqHistoricalDataInterval.Daily);
-			List<Data> stooqHistData = invfiz.collectData();
-			List<Data[]> matched = DataUtils.matchByDate(invfizPl, stooqHistData);
-			
-			// add latest
-			DataCollector latestFromStooq = new StooqDataCollector(fund.getStooq());
-			List<Data> stooqData = latestFromStooq.collectData();
-			stooqHistData.add(stooqData.get(0));
-			
-			// TODO: add today
-			String file = "output/" + fund.getStooq() + "_" + sdf.format(c1.getTime()) + ".csv";
-			toCsvFile(matched, file);
+			if (proceed(args, fund.getStooq())) {
+				DataCollector invfizInvestorsCollector = new InvestorsPlDataCollector(fund.getInvestorsPl());
+				List<Data> invfizPl = invfizInvestorsCollector.collectData();
+				Date start = invfizPl.get(0).getDate();
+				Date today = new Date(System.currentTimeMillis());
+				Date end = DateUtils.truncate(today, Calendar.DAY_OF_MONTH);
+				DataCollector invfiz = new StooqHistoricalDataCollector(fund.getStooq(), start, end, StooqHistoricalDataInterval.Daily);
+				List<Data> stooqHistData = invfiz.collectData();
+				List<Data[]> matched = DataUtils.matchByDate(invfizPl, stooqHistData);
+
+				// add latest
+				DataCollector latestFromStooq = new StooqDataCollector(fund.getStooq());
+				List<Data> stooqData = latestFromStooq.collectData();
+				stooqHistData.add(stooqData.get(0));
+
+				// TODO: add today
+				String file = "output/" + fund.getStooq() + "_" + sdf.format(c1.getTime()) + ".csv";
+				toCsvFile(matched, file);
+			}
 		}
 
 		// === silver
-		AllegroCoinsDataCollector allegroCoinsCollector = new AllegroCoinsDataCollector();
-		List<Data> allegroCoins = allegroCoinsCollector.collectData();
-		Date start = allegroCoins.get(0).getDate();
-		Date today = new Date(System.currentTimeMillis());
-		Date end = DateUtils.truncate(today, Calendar.DAY_OF_MONTH);
-		DataCollector rcsilaopenHistory = new StooqHistoricalDataCollector("rcsilaopen", start, end, StooqHistoricalDataInterval.Daily);
-		List<Data> stooqHistData = rcsilaopenHistory.collectData();
-		// add latest
-		DataCollector latestFromStooq = new StooqDataCollector("rcsilaopen");
-		List<Data> stooqData = latestFromStooq.collectData();
-		stooqHistData.add(stooqData.get(0));
-		
-		List<Data[]> matched = DataUtils.matchByDate(stooqHistData, allegroCoins);
-		String file = "output/" + "silver" + "_" + sdf.format(c1.getTime()) + ".csv";
-		toCsvFile(matched, file);
+		if (proceed(args, "silver")) {
+			AllegroCoinsDataCollector allegroCoinsCollector = new AllegroCoinsDataCollector();
+			List<Data> allegroCoins = allegroCoinsCollector.collectData();
+			Date start = allegroCoins.get(0).getDate();
+			Date today = new Date(System.currentTimeMillis());
+			Date end = DateUtils.truncate(today, Calendar.DAY_OF_MONTH);
+			DataCollector rcsilaopenHistory = new StooqHistoricalDataCollector("rcsilaopen", start, end, StooqHistoricalDataInterval.Daily);
+			List<Data> stooqHistData = rcsilaopenHistory.collectData();
+			// add latest
+			DataCollector latestFromStooq = new StooqDataCollector("rcsilaopen");
+			List<Data> stooqData = latestFromStooq.collectData();
+			stooqHistData.add(stooqData.get(0));
+
+			List<Data[]> matched = DataUtils.matchByDate(stooqHistData, allegroCoins);
+			String file = "output/" + "silver" + "_" + sdf.format(c1.getTime()) + ".csv";
+			toCsvFile(matched, file);
+		}
 
 		// === arkafrn12
-		DataCollector arkafrn12Collector = new ArkaDataCollector("arka-bz-wbk-fundusz-rynku-nieruchomosci-fiz");
-		List<Data> arkafrn = arkafrn12Collector.collectData();
-		start = arkafrn.get(0).getDate();
-		today = new Date(System.currentTimeMillis());
-		end = DateUtils.truncate(today, Calendar.DAY_OF_MONTH);
-		DataCollector arkafrn12History = new StooqHistoricalDataCollector("arkafrn12", start, end, StooqHistoricalDataInterval.Daily);
-		stooqHistData = arkafrn12History.collectData();
-		// add latest
-		latestFromStooq = new StooqDataCollector("arkafrn12");
-		stooqData = latestFromStooq.collectData();
-		stooqHistData.add(stooqData.get(0));
-		
-		matched = DataUtils.matchByDate(arkafrn, stooqHistData);
-		file = "output/" + "arkafrn12" + "_" + sdf.format(c1.getTime()) + ".csv";
-		toCsvFile(matched, file);
+		if (proceed(args, "arkafrn12")) {
+			DataCollector arkafrn12Collector = new ArkaDataCollector("arka-bz-wbk-fundusz-rynku-nieruchomosci-fiz");
+			List<Data> arkafrn = arkafrn12Collector.collectData();
+			Date start = arkafrn.get(0).getDate();
+			Date today = new Date(System.currentTimeMillis());
+			Date end = DateUtils.truncate(today, Calendar.DAY_OF_MONTH);
+			DataCollector arkafrn12History = new StooqHistoricalDataCollector("arkafrn12", start, end, StooqHistoricalDataInterval.Daily);
+			List<Data> stooqHistData = arkafrn12History.collectData();
+			// add latest
+			StooqDataCollector latestFromStooq = new StooqDataCollector("arkafrn12");
+			List<Data> stooqData = latestFromStooq.collectData();
+			stooqHistData.add(stooqData.get(0));
+
+			List<Data[]> matched = DataUtils.matchByDate(arkafrn, stooqHistData);
+			String file = "output/" + "arkafrn12" + "_" + sdf.format(c1.getTime()) + ".csv";
+			toCsvFile(matched, file);
+		}
 
 		System.out.println("Done.");
 	}
 	
+	private static boolean proceed(String[] args, String string) {
+		if (args.length == 0)
+			return true;
+		for (String arg : args) {
+			if (arg.equalsIgnoreCase(string)) 
+				return true;
+		}
+		return false;
+	}
+
 	private static void toCsvFile(List<Data[]> matched, String filePath) throws IOException {
 		BufferedWriter out = new BufferedWriter(new FileWriter(filePath));
 		for (Iterator<Data[]> iterator = matched.iterator(); iterator.hasNext();) {
